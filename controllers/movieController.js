6e691c44846e58bd74ff11b7ca21251ce8218dab
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-14 10:34:06 +0530

    Update movieController.js

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,21 +1,67 @@
+// const Movie = require('../models/Movie');
+
+// exports.getAllMovies = async (req, res) => {
+//   try {
+//     // TODO: Fetch all movies from the database
+//     // TODO: Send a JSON response with the movies
+//     // res.status(200).json(movies);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
+// exports.upvoteMovie = async (req, res) => {
+//   try {
+//     // TODO: Retrieve the movie ID from the request parameters
+//     // TODO: Update the movie's upvotes using $inc and retrieve the updated movie
+//     // TODO: Send a success response with the upvoted movie details
+//     // res.status(200).json({ message: 'Movie upvoted successfully', upvotedMovie: movie });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
+
+// exports.downvoteMovie = async (req, res) => {
+//   try {
+//     // TODO: Retrieve the movie ID from the request parameters
+//     // TODO: Update the movie's downvotes using $inc and retrieve the updated movie
+//     // TODO: Send a success response with the downvoted movie details
+//     // res.status(200).json({ message: 'Movie downvoted successfully', downvotedMovie: movie });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 const Movie = require('../models/Movie');
 
 exports.getAllMovies = async (req, res) => {
   try {
-    // TODO: Fetch all movies from the database
-    // TODO: Send a JSON response with the movies
-    // res.status(200).json(movies);
+    // Fetch all movies from the database
+    const movies = await Movie.find();
+
+    // Send a JSON response with the movies
+    res.status(200).json(movies);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
 exports.upvoteMovie = async (req, res) => {
   try {
-    // TODO: Retrieve the movie ID from the request parameters
-    // TODO: Update the movie's upvotes using $inc and retrieve the updated movie
-    // TODO: Send a success response with the upvoted movie details
-    // res.status(200).json({ message: 'Movie upvoted successfully', upvotedMovie: movie });
+    // Retrieve the movie ID from the request parameters
+    const movieId = req.params.movieId;
+
+    // Update the movie's upvotes using $inc and retrieve the updated movie
+    const movie = await Movie.findByIdAndUpdate(
+      movieId,
+      { $inc: { upvotes: 1 } },
+      { new: true }
+    );
+
+    // Send a success response with the upvoted movie details
+    res.status(200).json({ message: 'Movie upvoted successfully', upvotedMovie: movie });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
@@ -24,10 +70,18 @@ exports.upvoteMovie = async (req, res) => {
 
 exports.downvoteMovie = async (req, res) => {
   try {
-    // TODO: Retrieve the movie ID from the request parameters
-    // TODO: Update the movie's downvotes using $inc and retrieve the updated movie
-    // TODO: Send a success response with the downvoted movie details
-    // res.status(200).json({ message: 'Movie downvoted successfully', downvotedMovie: movie });
+    // Retrieve the movie ID from the request parameters
+    const movieId = req.params.movieId;
+
+    // Update the movie's downvotes using $inc and retrieve the updated movie
+    const movie = await Movie.findByIdAndUpdate(
+      movieId,
+      { $inc: { downvotes: 1 } },
+      { new: true }
+    );
+
+    // Send a success response with the downvoted movie details
+    res.status(200).json({ message: 'Movie downvoted successfully', downvotedMovie: movie });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
